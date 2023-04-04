022b513f8a939e8c32035fd267be3381a521fce0
Jack Dougherty <jack.dougherty@trincoll.edu>
2023-04-04 09:49:09 -0400

    updated crash data and dates

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function tsToDate(ts) {
 
 // display initial data, where Jan = 0 and Dec = 11
 var initFrom = dateToTS(new Date(2020, 0, 1));
-var initTo = dateToTS(new Date(2023, 2, 22));
+var initTo = dateToTS(new Date(2023, 3, 1));
 
 Papa.parse('./data/crashes.csv', {
     download: true,
@@ -92,7 +92,7 @@ Papa.parse('./data/crashes.csv', {
             var crashesFiltered = crashes.filter(function (point) {
                 return (($('#localStateUS').prop('checked') ? point.r != 1 : false)
                     || ($('#interstate').prop('checked') ? point.r === 1 : false))
-                    
+
                     && (($('#vehiclesOnly').prop('checked') ? (point.c === 0 && point.p === 0) : false)
                         || ($('#cyclists').prop('checked') ? point.c === 1 : false)
                         || ($('#pedestrians').prop('checked') ? point.p === 1 : false))
@@ -173,7 +173,7 @@ Papa.parse('./data/crashes.csv', {
 
             // set full range display, where 0 = Jan and 11 = Dec
             min: dateToTS(new Date(2015, 0, 1)),
-            max: dateToTS(new Date(2023, 3, 22)),
+            max: dateToTS(new Date(2023, 3, 1)),
 
             from: initFrom,
             to: initTo,
