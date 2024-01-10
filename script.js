30f5465a9c4b01ba5c4664c8accac426eab537d8
Jack Dougherty <jack.dougherty@trincoll.edu>
2024-01-10 11:12:23 -0500

    updated and processed data to 1 Jan 2024

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function tsToDate(ts) {
 
 // display initial data, where Jan = 0 and Dec = 11
 var initFrom = dateToTS(new Date(2020, 0, 1));
-var initTo = dateToTS(new Date(2023, 7, 1));
+var initTo = dateToTS(new Date(2024, 0, 1));
 
 Papa.parse('./data/crashes.csv', {
     download: true,
@@ -173,7 +173,7 @@ Papa.parse('./data/crashes.csv', {
 
             // set full range display, where 0 = Jan and 11 = Dec
             min: dateToTS(new Date(2015, 0, 1)),
-            max: dateToTS(new Date(2023, 7, 1)),
+            max: dateToTS(new Date(2024, 0, 1)),
 
             from: initFrom,
             to: initTo,
